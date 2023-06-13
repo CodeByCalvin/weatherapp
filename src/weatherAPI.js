950d66ab3c019166ce215baf91297148f1b2fefb
Calvin Merry <calvin@calvinmerry.com>
2023-06-13 11:13:51 +0100

    Minor changes

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -10,7 +10,6 @@ export default class WeatherAPI {
         `https://api.openweathermap.org/data/3.0/onecall?lat=53.383331&lon=-1.466667&exclude=minutely,hourly,alerts&appid=${this.apiKey}`
       );
       const data = response.data;
-      console.log(data);
       return data;
     } catch (error) {
       console.log(error);
