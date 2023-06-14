f731e08f8076db6b288fa75a2c026b718019c64b
Calvin Merry <calvin@calvinmerry.com>
2023-06-14 10:52:43 +0100

    Updated the css so there's a fixed height for the description which then doesn't overflow into the rest of the card

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default class WeatherAPI {
   async getWeather() {
     try {
-      const url = `https://api.openweathermap.org/data/3.0/onecall?lat=53.383331&lon=-1.466667&exclude=minutely,hourly,alerts&appid=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`;
+      const url = `https://api.openweathermap.org/data/3.0/onecall?lat=53.383331&lon=-1.466667&exclude=minutely,hourly,alerts&appid=069cb4e68b356cd303ad9ce58a685559`;
       const response = await axios.get(url);
       const data = response.data;
       return data;
